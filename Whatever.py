2e6f457af5d5eeca0008f4b9fa849eabb709de61
Genericusername6 <eet87202@gmail.com>
2024-10-29 01:30:35 +0000

    time, marquee, anything

--- Whatever.py
+++ Whatever.py
@@ -4,5 +4,12 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def whatever():
-    return flask.render_template("something.html")
+    return flask.render_template("something.html", x=30)
 
+@app.route("/anything")
+def where_ever():
+    return flask.render_template("anything.html")
+
+@app.route("/<file>.<ext>")
+def serveCss(file, ext):
+    return app.send_static_file(f'{file}.{ext}')
